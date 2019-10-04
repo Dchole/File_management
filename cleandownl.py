2b151359ccbed3897548ae2661dddbfc08a0c093
Dchole <derekoware47@gmail.com>
2019-10-05 00:05:58 +0100

    complete

--- cleandownl.py
+++ cleandownl.py
@@ -1,13 +1,26 @@
 import os
 import time
+import file_extensions
 
+target_directory = '/mnt/c/Users/KOMFUEKU METH. PRY/Downloads'
 
-target_directory = '/mnt/c/Users/KOMFUEKU METH. PRY/Projects/demos/cat-test'
+files = [file for file in os.listdir(
+    target_directory) if os.path.isfile(os.path.join(target_directory, file))]
 
+for f in files:
+    extension = os.path.splitext(f)[1]
 
-def categorize(target_directory):
-    for file in os.listdir(target_directory):
-        print(file)
+    try:
+        extension = file_extensions.extensions[extension]
+    except KeyError:
+        pass
 
+    if os.path.exists(target_directory + "/" + extension):
+        print(extension, "Folder exists")
+        pass
+    else:
+        os.mkdir(target_directory + "/" + extension)
+        print(extension, "Folder created")
 
-categorize(target_directory)
+    os.rename(target_directory + "/" + f,
+              target_directory + "/" + extension + "/" + f)
